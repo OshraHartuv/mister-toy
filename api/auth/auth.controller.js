d9812c67be591e397d11aa9e0d7ec867a0ec6d3e
Oshra Hartuv <oshrartuv@gmail.com>
2021-11-29 20:26:44 +0200

    fixed http

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -4,9 +4,10 @@ const logger = require('../../services/logger.service')
 async function login(req, res) {
     const { username, password } = req.body
     try {
+        console.log(`user: ${username}, ${password}`);
         const user = await authService.login(username, password)
         req.session.user = user
-        res.json(user)
+        res.send(user)
     } catch (err) {
         logger.error('Failed to Login ' + err)
         res.status(401).send({ err: 'Failed to Login' })
