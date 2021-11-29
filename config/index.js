da7d0d314a2356cc86c071f97e6c10ef83580047
Oshra Hartuv <oshrartuv@gmail.com>
2021-11-29 19:38:17 +0200

    httpservice added

--- config/index.js
+++ config/index.js
@@ -6,10 +6,10 @@ if (process.env.NODE_ENV === 'production') {
   // we are in production - return the prod set of keys
   config = require('./prod')
 } else {
-  // config = require('./prod')
+  config = require('./prod')
 
   // we are in development - return the dev keys!!!
-  config = require('./dev')
+  // config = require('./dev')
 }
 
 module.exports = config
